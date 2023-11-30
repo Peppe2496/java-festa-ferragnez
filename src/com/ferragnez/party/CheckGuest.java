053fda3ed935a2a5fa3a31590c0122a5e5bf6b2b
Peppe2496 <giuseppemusellajob@gmail.com>
2023-11-30 14:55:40 +0100

    ended

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,2 +1,42 @@
-package com.ferragnez.party;public class CheckGuest {
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] guests = {
+                "Dua Lipa",
+                "Paris Hilton",
+                "Manuel Agnelli",
+                "J-Ax",
+                "Francesco Totti",
+                "Ilary Blasi",
+                " Bebe Vio",
+                "Luis",
+                "Pardis Zarei",
+                "Martina Maccherone",
+                "Rachel Zeilic"
+        };
+
+        System.out.println("What's your name?");
+        String guestName = scanner.nextLine();
+
+        boolean accepted = false;
+
+        for (int i = 0; i < guests.length && !accepted ; i++) {
+
+            if (guestName.equals(guests[i])){
+                accepted=true;
+                break;
+            }
+        }
+        if (accepted){
+            System.out.println("Welcome");
+        }else {
+            System.out.println("Sorry, you are not on the list");
+        }
+
+        scanner.close();
+    }
 }
